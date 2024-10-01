bf2d476fa25b1d7007294f24bcb8409041f59f56
Cristobal Grana <cristobal.wee@gmail.com>
2024-09-30 21:11:30 -0700

    Added lenis for smooth scrolling

--- main.js
+++ main.js
@@ -1,26 +1,51 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import Logo from './assets/logo-dark.svg';
+import Phone from './assets/phone.svg';
+import Instagram from './assets/instagram.svg';
+import Lenis from 'lenis';
+import { gsap } from 'gsap';
+
+import './styles/style.scss';
+import 'lenis/dist/lenis.css';
 
 // https://github.com/lukePeavey/SplitType
 
+const lenis = new Lenis();
+function raf(time) {
+    lenis.raf(time);
+    requestAnimationFrame(raf);
+}
+
+requestAnimationFrame(raf);
+
 document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
+  <nav>
+    <img src="${Logo}" class='logo' alt='Bonno bartending logo' />
+    <div class='btn-container'>
+      <button class='btn-primary'>
+        <img src="${Phone}" class='logo' alt='Bonno bartending logo' />
+        +1 (480) 707-3769
+      </button>
+      <button class='btn-primary btn-primary-circle'>
+        <img src="${Instagram}" class='logo' alt='Bonno bartending logo' />
+      </button>
     </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
+  </nav>
+  <div class='intro'>
+    <p>Serving Phoenix, AZ</p>
+    <h1>You provide the liquor, we’ll mix it – <span class='strong'>pro bono</span>.</h1>
+    <button class='btn-primary btn-primary-large' type="button">Book us</button>
+    <div class='image fast-scroll'></div>
   </div>
 `
 
-setupCounter(document.querySelector('#counter'))
+const fastScrollElement = document.querySelector('.fast-scroll');
+const speedMultiplier = 1.3;
+
+lenis.on('scroll', ({ scroll }) => {
+  console.log(fastScrollElement);
+  const offset = scroll * speedMultiplier;
+
+  if (fastScrollElement) fastScrollElement.style.transform = `translateY(-${offset}px)`;
+});
+
+lenis.start();
